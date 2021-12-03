aafda36118527b824ae8cfa44c4ced0e95fa38df
Ed <edmilsonrobson@gmail.com>
2021-12-03 12:51:10 -0300

    Add Comment types

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -150,16 +150,55 @@ interface ICannyLabel {
 }
 
 interface ICannyChangelogEntry {
+  /** A unique identifier for the entry. */
   id: string;
+  /** Time at which the entry was first created, in ISO 8601 format. */
   created: Date;
+  /** The list of labels that the entry is associated with. */
   labels: ICannyLabel[];
+  /** Time at which the entry was last updated, in ISO 8601 format. */
   lastSaved: Date;
+  /** The markdown contents of the entry. */
   markdownDetails: string;
+  /** The plaintext contents of the entry, with images, videos, and links stripped. */
   plaintextDetails: string;
+  /** The list of posts this entry is linked to. */
   posts: ICannyPost[];
+  /** Time at which the entry was published, if it has been published. */
   publishedAt: Date | null;
+  /** Time at which the entry is schedule to be published, if it is scheduled. */
   scheduledFor: Date | null;
+  /** The status of the entry, describing whether it has been published. Will be set to draft, scheduled, or published. */
   status: 'draft' | 'scheduled' | 'published';
+  /** The title of the entry. */
+  title: string;
+  /** The list of types associated with the entry. Can include new, improved, or fixed. */
   types: CannyChangelogEntryType[];
+  /** The public URL to the entry page on Canny. */
   url: string;
+}
+
+interface ICannyComment {
+  /** A unique identifier for the comment. */
+  id: string;
+  /** The user who created the comment. */
+  author: ICannyUser;
+  /** The board the comment is associated with. */
+  board: ICannyBoard;
+  /** Time at which the comment was created, in ISO 8601 format. */
+  created: Date;
+  /** An array of the URLs of the images associated with this comment. */
+  imageURLs: string[];
+  /** Whether or not the comment is an internal comment. */
+  internal: boolean;
+  /** The number of likes a comment has received. */
+  likeCount: number;
+  /** An array of user objects who are mentioned in the comment. */
+  mentions: ICannyUser[];
+  /** The id of the comment that this comment is a reply to. If this comment is not a reply, this field will be null. */
+  parentID: string | null;
+  /** The post the comment is associated with. */
+  post: ICannyPost;
+  /** The text value of this comment.  */
+  value: string;
 }
